536b3ecdb521b356103b908cb04b134d14182e4b
jakemal <maldonadojm@s.dcsdk12.org>
2015-12-09 10:57:57 -0700

    GOT THE WINDOW. now for everything else

--- main.rb
+++ main.rb
@@ -19,28 +19,25 @@ class Main < Gosu::Window
 	end
 
 	def update
-		 return if @timer.game_over?
-		 return if @player.death
+	  # return if @timer.game_over?
 
 		@player.go_left if Gosu::button_down? Gosu::KbLeft
 		@player.go_right if Gosu::button_down? Gosu::KbRight
 
-		@player.move
+		
 
 	end
 
 	def draw 
 		@player.draw
-		@background_image.draw(0, 0, 0,  0)
-		@font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
-		@font.draw("#{@timer.print_out}", 500, 10, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
+		# @background_image.draw(0, 0, 0,  0)
+		# @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
+		# @font.draw("#{@timer.print_out}", 500, 10, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
 	end
 
 	def button_down(id)
 		close if id == Gosu::KbEscape		
 	end
-
-
 
 end
 
--- timer.rb
+++ timer.rb
@@ -1,26 +1,26 @@
-require "gosu"
-require_relative 'z_order'
-require_relative 'player'
+# require "gosu"
+# require_relative 'z_order'
+# require_relative 'player'
 
-class Timer	
-	attr_reader :time_left
+# class Timer	
+# 	attr_reader :time_left
 
-	def initialize(player)
-		@time_left = 30
+# 	def initialize(player)
+# 		@time_left = 30
 
-		@player = player
-	end
+# 		@player = player
+# 	end
 
-	def print_out
-		if @time_left > 1 
-			 @time_left -= 0.01
-			 "Time left: #{@time_left.truncate}"
-		else
-			 "game over"
-		end
-	end
+# 	def print_out
+# 		if @time_left > 1 
+# 			 @time_left -= 0.01
+# 			 "Time left: #{@time_left.truncate}"
+# 		else
+# 			 "game over"
+# 		end
+# 	end
 
-	def game_over?
-		@time_left <= 1
-	end
-end
+# 	def game_over?
+# 		@time_left <= 1
+# 	end
+# end
